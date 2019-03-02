adc02a25732fb9ac031eb34eacebc8aeaefe7c72
Philip Poppler <philpoppler@gmail.com>
2019-03-01 23:21:52 -0800

    Create Express App

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Connecting to port ${PORT}`);
+    (err) => {
+        console.log('Server failed to connect');
+    }
+    });
+
+app.get('/', function(req,res){
+    res.send('testing');
+});
+

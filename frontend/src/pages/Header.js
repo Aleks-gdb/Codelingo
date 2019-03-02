7e478a9498201bac049fe7f71e3514ad4a85690c
elizabethhim <elizabethxhim@gmail.com>
2019-03-02 08:36:37 -0800

    Add header

---
+++ frontend/src/pages/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import space from "../images/Space_background.jpg";
+export default class Header extends Component {
+    render() {
+        return (
+            <div id ="header">
+            </div>
+        );
+    }
+}

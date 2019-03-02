b124efe715b461b3ccf85f0576dea8cc6739a9d4
elizabethhim <elizabethxhim@gmail.com>
2019-03-01 23:41:24 -0800

    Add navbar collapse functionality

--- frontend/src/Components/Navigation.js
+++ frontend/src/Components/Navigation.js
@@ -11,16 +11,20 @@ import {
 export default class Navigation extends Component {
 	constructor(props) {
 		super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+          isOpen: false
+        };
     }
-    toggle() {
+      toggle() {
         this.setState({
-            isOpen: !this.state.isOpen
+          isOpen: !this.state.isOpen
         });
     }
     render() {
         return (
             <div>
-                <Navbar color = "dark">
+                <Navbar color = "dark" dark expand="md">
                     <NavbarBrand href ="/">Codelingo</NavbarBrand>
                     <NavbarToggler onClick={this.toggle} />
                     <Collapse isOpen={this.state.isOpen} navbar>

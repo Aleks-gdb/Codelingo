851265e2c12ac3b149b07d4e7cc9bec08052a17c
Breeland Abrams <breeland.abrams@gmail.com>
2019-03-02 09:56:24 -0800

    Editied logo and hoempage css

--- frontend/src/Components/Navigation.js
+++ frontend/src/Components/Navigation.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Container, Row, Col, Image, Button } from "react-bootstrap";
 import "../css/Navigation.css";
-import logo from "../images/codelingo logo.png";
+import logo from "../images/codelingowo.png";
 export default class Navigation extends Component {
   constructor(props) {
     super(props);
@@ -21,7 +21,7 @@ export default class Navigation extends Component {
       <Container className="fullscreen" style={{ backgroundColor: "black" }}>
         <Row className="navBar">
           <Col md={6}>
-            <Image src={logo} width="200" height="75" />
+            <Image id="logo" src={logo} width="300" height="75" />
           </Col>
           <Col md={6}>
             <Button id="loginButton" variant="success" size="lg">
